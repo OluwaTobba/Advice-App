eb12f20d0593f4cd2a17bc209f1c3f35499ceea2
OluwaTobba <onaopemipomichael20@gmail.com>
2024-04-21 13:05:58 +0100

    commit-tobe edited

---
+++ main.js
@@ -0,0 +1,9 @@
+// Function to open the modal
+function openModal(modalId) {
+    document.getElementById(modalId).style.display = "block";
+}
+
+// Function to close the modal
+function closeModal(modalId) {
+    document.getElementById(modalId).style.display = "none";
+}

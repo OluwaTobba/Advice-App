719466eec8f1e549ff928213e1717a26864e01d6
OluwaTobba <142856021+OluwaTobba@users.noreply.github.com>
2024-05-04 14:15:27 +0100

    quotes page updated

--- quotes.js
+++ quotes.js
@@ -1,7 +1,44 @@
 const quotes = [
+    // Inspiration and Motivation
     {
         text: "The only way to do great work is to love what you do.",
         author: "Steve Jobs"
+    },
+    {
+        text: "Believe you can and you're halfway there.",
+        author: "Theodore Roosevelt"
+    },
+    {
+        text: "Success is not final, failure is not fatal: It is the courage to continue that counts.",
+        author: "Winston Churchill"
+    },
+    {
+        text: "The future belongs to those who believe in the beauty of their dreams.",
+        author: "Eleanor Roosevelt"
+    },
+    {
+        text: "In the middle of difficulty lies opportunity.",
+        author: "Albert Einstein"
+    },
+    {
+        text: "The only limit to our realization of tomorrow will be our doubts of today.",
+        author: "Franklin D. Roosevelt"
+    },
+    {
+        text: "Your time is limited, don't waste it living someone else's life.",
+        author: "Steve Jobs"
+    },
+    {
+        text: "Dream big and dare to fail.",
+        author: "Norman Vaughan"
+    },
+    {
+        text: "The only impossible journey is the one you never begin.",
+        author: "Tony Robbins"
+    },
+    {
+        text: "You are never too old to set another goal or to dream a new dream.",
+        author: "C.S. Lewis"
     },
 ];
 
